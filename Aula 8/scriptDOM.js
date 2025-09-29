31d5c4df0114f94f64a02c75fc07cb49554be08e
DANIEL ROBSON ALEXANDRE SILVA <daniel.silva067@cs.cruzeirodosul.edu.br>
2025-09-29 20:56:10 -0300

    Atualizando

--- Aula 8/scriptDOM.js
+++
@@ -1,6 +0,0 @@
-// Document Object Model (DOM)
-// Manipulação de elementos HTML com JavaScript 
-// Selecionar elementos
-// Alterar conteúdo e estilo
-// Adicionar e remover elementos
-// Eventos e interatividade 
