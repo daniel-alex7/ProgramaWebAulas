b4322cd1a78d4b73eda6b6e93c2cda38ab274e1c
DANIEL ROBSON ALEXANDRE SILVA <daniel.silva067@cs.cruzeirodosul.edu.br>
2025-11-03 21:17:33 -0300

    Exercicio

--- Aula 11/script.js
+++ Aula 11/script.js
@@ -4,6 +4,7 @@ fetch("dadosAlunos.json").then((response) =>{
 
             //Formatação da data
             
+            const idade = new Date () - new Date (aluno.nascimento)
 
             const nasc = new Date (`${aluno.nascimento}`)
             const nascFormatado = nasc.toLocaleDateString('pt-Br', {timeZone: 'UTC', })
@@ -15,6 +16,7 @@ fetch("dadosAlunos.json").then((response) =>{
                        <td>${nascFormatado}</td>
                        <td>${aluno.email}</td>
                        <td>${aluno.cpf}</td>
+                       <td> ${(idade / (365.25 * 24 * 60 * 60 * 1000)).toFixed(0)} anos </td>
                     </tr>`
             
         });
