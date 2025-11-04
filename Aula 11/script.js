12a6d03a81e8b0c4844c46375f87b28668804d8d
DANIEL ROBSON ALEXANDRE SILVA <daniel.silva067@cs.cruzeirodosul.edu.br>
2025-11-03 21:02:39 -0300

    Atualizando

--- Aula 11/script.js
+++ Aula 11/script.js
@@ -1,5 +1,21 @@
 fetch("dadosAlunos.json").then((response) =>{
     response.json().then((data)=>{
-        console.log(data.alunos)
+        data.alunos.forEach(aluno => {
+
+            //Formatação da data
+            
+
+            const nasc = new Date (`${aluno.nascimento}`)
+            const nascFormatado = nasc.toLocaleDateString('pt-Br', {timeZone: 'UTC', })
+
+            corpoDaTabela.innerHTML += 
+                    `<tr>
+                       <td>${aluno.nome}</td>
+                       <td>${aluno.nascimento}</td>
+                       <td>${nascFormatado}</td>
+                       <td>${aluno.email}</td>
+                    </tr>`
+            
+        });
     })
 })
